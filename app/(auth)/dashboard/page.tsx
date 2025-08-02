e0d0c7b0b9621a4e37e656c6c49a82edc736bca4
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-08-02 15:59:29 +0600

    feat(dashboard): implement role-based dashboard routing with admin and user views

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function DashboardPage() {
-	return <div>DashboardPage</div>;
+import { Role } from "@/types/user";
+import { getUser } from "@/actions/auth";
+import AdminDashboard from "./admin-dashboard";
+import UserDashboard from "./user-dashboard";
+import { redirect } from "next/navigation";
+
+export default async function page() {
+	const user = await getUser();
+	if (!user) redirect("/logout");
+
+	return user?.role === Role.Admin ? <AdminDashboard /> : <UserDashboard />;
 }
