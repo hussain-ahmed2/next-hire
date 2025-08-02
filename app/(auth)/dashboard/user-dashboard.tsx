841513e73fa5d3d4afd2cf4d0e5262d4da54b1ad
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-08-02 17:05:10 +0600

    Add user prop to UserDashboard component and display user data

--- app/(auth)/dashboard/user-dashboard.tsx
+++ app/(auth)/dashboard/user-dashboard.tsx
@@ -1,3 +1,9 @@
-export default function UserDashboard() {
-	return <div>UserDashboard</div>;
+import { IUser } from "@/types/user";
+
+export default function UserDashboard({ user }: { user: IUser }) {
+	return (
+		<div>
+			<pre>{JSON.stringify(user, null, 2)}</pre>
+		</div>
+	);
 }
