00e4476f26919ba495ac3d962b487a35869e9207
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-08-02 15:58:51 +0600

    feat(types): update IUser interface with _id, slug, and bio fields

--- types/user.ts
+++ types/user.ts
@@ -5,11 +5,14 @@ export enum Role {
 }
 
 export interface IUser {
+	_id: string;
+	slug: string;
 	name: string;
 	email: string;
 	password: string;
-	avatar: string;
+	avatar?: string;
 	role: Role;
+	bio?: string;
 	createdAt: Date;
 	updatedAt: Date;
 }
