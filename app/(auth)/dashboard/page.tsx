ae0081238b32052c854f13288370129c30a1b1b9
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-08-04 23:54:59 +0600

    refactor: enhance dashboard routing and role handling
    - Add TabType for candidate dashboard navigation
    - Support searchParams for tab-based routing
    - Replace UserDashboard with CandidateDashboard
    - Add placeholder for Employer dashboard
    - Improve role-based dashboard rendering

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,12 +1,16 @@
 import { Role } from "@/types/user";
 import { getUser } from "@/actions/auth";
 import AdminDashboard from "./(admin)/admin-dashboard";
-import UserDashboard from "./user-dashboard";
 import { redirect } from "next/navigation";
+import CandidateDashboard from "./(candidate)/candidate-dashboard";
 
-export default async function page() {
+export type TabType = "profile" | "edit-profile" | "applied-jobs" | "saved-jobs";
+
+export default async function page({ searchParams }: { searchParams: Promise<{ tab?: string }> }) {
 	const user = await getUser();
 	if (!user) redirect("/logout");
 
-	return user?.role === Role.Admin ? <AdminDashboard user={user} /> : <UserDashboard user={user} />;
+	const { tab = "profile" } = (await searchParams) as { tab?: TabType };
+
+	return user?.role === Role.Admin ? <AdminDashboard user={user} /> : user.role === Role.Employer ? <div>Employer Dashboard</div> : <CandidateDashboard tab={tab} user={user} />;
 }
