790ee7e2ec3b27f459d5f97b539b9cb20216da4a
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-08-04 23:55:15 +0600

    remove: delete user-dashboard.tsx
    - Replaced by role-specific candidate dashboard
    - Part of dashboard refactoring to separate candidate and employer views

--- app/(auth)/dashboard/user-dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { IUser } from "@/types/user";
-
-export default function UserDashboard({ user }: { user: IUser }) {
-	return (
-		<div>
-			<pre>{JSON.stringify(user, null, 2)}</pre>
-		</div>
-	);
-}
