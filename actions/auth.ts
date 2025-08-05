88fbf838d86de87dcf27673b3a8572d68e372b15
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-08-06 00:13:49 +0600

    feat(auth): add user profile update functionality with password validation

--- actions/auth.ts
+++ actions/auth.ts
@@ -2,8 +2,10 @@
 
 import connectDB from "@/lib/db";
 import { validateToken } from "@/lib/jwt";
+import { EditProfileSchema, editProfileSchema, getErrors } from "@/lib/validation-schema";
 import User, { UserDocument } from "@/models/User";
 import { IUser } from "@/types/user";
+import bcrypt from "bcryptjs";
 import { cookies } from "next/headers";
 
 export async function getUser(): Promise<IUser | null> {
@@ -18,7 +20,7 @@ export async function getUser(): Promise<IUser | null> {
 
 		await connectDB();
 
-		const user: UserDocument = await User.findById(id).select("-password");
+		const user: UserDocument | null = await User.findById(id);
 		if (!user) return null;
 
 		return {
@@ -48,5 +50,54 @@ export async function logout() {
 	} catch (error) {
 		console.error(error);
 		return false;
+	}
+}
+
+type EditProfileErrors = {
+	name?: string;
+	email?: string;
+	bio?: string;
+	oldPassword?: string;
+	newPassword?: string;
+	avatar?: string;
+};
+
+export type EditProfileState = {
+	success: boolean;
+	errors?: EditProfileErrors;
+	form?: EditProfileSchema;
+};
+
+export async function updateUser(prevState: EditProfileState, formData: FormData) {
+	try {
+		const credentials = Object.fromEntries(formData) as EditProfileState["form"];
+		const { success, data, error } = await editProfileSchema.safeParseAsync(credentials);
+
+		if (!success) {
+			return { ...prevState, success: false, errors: getErrors(error), form: credentials };
+		}
+
+		await connectDB();
+
+		const user = await getUser();
+
+		if (!user) {
+			return { ...prevState, success: false, errors: { email: "User not found" }, form: data };
+		}
+
+		if (data?.oldPassword && !(await bcrypt.compare(data.oldPassword, user.password))) {
+			return { ...prevState, success: false, errors: { oldPassword: "Incorrect password" }, form: data };
+		}
+
+		const updatedUser = await User.findOneAndUpdate({ _id: user._id }, data, { new: true, upsert: true });
+
+		if (!updatedUser) {
+			return { ...prevState, success: false, errors: { email: "Failed to update user" }, form: data };
+		}
+
+		return { ...prevState, success: true, form: data };
+	} catch (error) {
+		console.error(error);
+		return { ...prevState, success: false };
 	}
 }
