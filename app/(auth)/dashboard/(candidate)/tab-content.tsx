e4694153076009fcd830802ffde68cd8f28caf15
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-08-06 00:14:17 +0600

    feat(tab-content): implement tab content components with scroll styling and proper imports

--- app/(auth)/dashboard/(candidate)/tab-content.tsx
+++ app/(auth)/dashboard/(candidate)/tab-content.tsx
@@ -1,10 +1,14 @@
 import { IUser } from "@/types/user";
 import { TabType } from "../page";
+import EditProfile from "./(tab-content)/edit-profile";
+import AppliedJobs from "./(tab-content)/applied-jobs";
+import SavedJobs from "./(tab-content)/saved-jobs";
+import Profile from "./(tab-content)/profile";
 
 export default function TabContent({ user, tab }: { user: IUser; tab?: TabType }) {
 	return (
-		<div className="flex-1 flex flex-col p-4">
-			{tab === "edit-profile" ? <div>Edit Profile</div> : tab === "applied-jobs" ? <div>Applied Jobs</div> : tab === "saved-jobs" ? <div>Saved Jobs</div> : <div>Profile</div>}
+		<div className="flex-1 flex flex-col p-4 h-full max-h-[calc(100vh-12.75rem)] overflow-y-scroll scrollbar-hide">
+			{tab === "edit-profile" ? <EditProfile user={user} /> : tab === "applied-jobs" ? <AppliedJobs /> : tab === "saved-jobs" ? <SavedJobs /> : <Profile user={user} />}
 		</div>
 	);
 }
